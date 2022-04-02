4cc4d2362ef93d4d8db4623e052b8b135c30d5f1
ARUSD <asd123fdssal@naver.com>
2022-04-02 19:35:47 +0900

    Spring Security 설정 변경
    자바스크립트 사용을 위해서 csrf 토큰 비활성화

--- src/main/java/com/arsud/sdmc_spring_web_project/config/SpringSecurityConfig.java
+++ src/main/java/com/arsud/sdmc_spring_web_project/config/SpringSecurityConfig.java
@@ -25,7 +25,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic().disable();
-        http.csrf();
+        http.csrf().disable();
         http.rememberMe();
 
         http.authorizeRequests()
